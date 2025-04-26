d93c50cb6259ac90dd6524ea0c58141ce8b93b54
Tönjes <toenjes@BookMac-Pro-von-Tonjes.local>
2025-04-26 14:37:38 +0200

    mousebrain Sachen oder so

--- scripts/mousebrain.gd
+++ scripts/mousebrain.gd
@@ -6,6 +6,10 @@ var drop_spots = []
 
 var node_being_dragged: Node = null
 
+func spawn_cards() -> void:
+	spawn_player_card()
+	spawn_enemy_card()
+
 func spawn_player_card() -> void:
 	var new_card = card_prefab.instantiate()
 	new_card.got_dropped.connect(hand.card_got_dropped)
@@ -15,3 +19,9 @@ func spawn_player_card() -> void:
 	get_tree().get_root().get_child(0).add_child(new_card)
 	new_card.global_position = Vector2(0,0)
 	hand.add_card(new_card)
+
+func spawn_enemy_card() -> void:
+	var new_card = card_prefab.instantiate()
+	get_tree().get_root().get_child(0).add_child(new_card)
+	new_card.global_position = Vector2(0,0)
+	enemy_hand.add_card(new_card)
