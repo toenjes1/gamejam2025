98447fc78c6ceb3c87e126dde5e9f86e56e1d38c
MaximStanko <f.maxim.stanko@gmail.com>
2025-04-27 17:04:28 +0200

    ass

--- scripts/global.gd
+++ scripts/global.gd
@@ -1,7 +1,7 @@
 extends Node
 
-var player_seed = 69
-var enemy_seed = 457
+var player_seed = 70
+var enemy_seed = 45
 
 const card_base_path = 'res://assets/images/cards/'
 
@@ -32,8 +32,8 @@ const no_card_texture = preload(card_base_path + "no_card.png")
 var numb_card_ids = []
 var spec_card_ids = []
 
-var player_bag = ['swa']
-var enemy_bag = ['1','1','1','1','1']
+var player_bag = []
+var enemy_bag = []
 
 const card_prefab = preload("res://prefabs/card.tscn")
 var hand = null
@@ -142,7 +142,7 @@ func spawn_player_card() -> Node:
 func spawn_enemy_card() -> Node:
 	if enemy_bag.is_empty():
 		enemy_bag = create_bag(enemy_seed)
-	var new_card = spawn_card(enemy_bag[0], false)
+	var new_card = spawn_card(enemy_bag[0], true)
 	enemy_bag.remove_at(0)
 	new_card.is_player_card = false
 	enemy_hand.add_card(new_card)
